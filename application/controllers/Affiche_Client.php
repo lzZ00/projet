bbb4fbe42be46032c112ad2536b9ee821c90fbfc
zherui <zherui.li@outlook.com>
2017-04-09 20:22:29 +0200

    supprime test

--- application/controllers/Affiche_Client.php
+++ application/controllers/Affiche_Client.php
@@ -11,25 +11,14 @@ class Affiche_Client extends CI_Controller {
     public function index()
     {
         $data['client'] = $this->Affiche_Client_Model->get_client();
+        if (empty($data['client']))
+        {
+            show_404();
+        }
         $data['title'] = 'Information de Client';
         $this->load->view('templates/header', $data);
         $this->load->view('Affiche_Client/index', $data);
         $this->load->view('templates/footer');
     }
 
-    public function view($slug = NULL)
-    {
-        $data['unclient'] = $this->Affiche_Client->get_news($slug);
-
-        if (empty($data['unclient']))
-        {
-            show_404();
-        }
-
-        $data['title'] = $data['unclient']['title'];
-
-        $this->load->view('templates/header', $data);
-        $this->load->view('Affiche_Client/view', $data);
-        $this->load->view('templates/footer');
-    }
 }
