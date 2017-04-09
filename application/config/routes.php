6c85f8057ebe9b003e520b825673ce328216aae8
lzZ00 <310449968@qq.com>
2017-04-09 22:14:56 +0800

    app

--- application/config/routes.php
+++ application/config/routes.php
@@ -48,7 +48,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
-*/
+*//*
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+*/
+/*
+$route['default_controller'] = 'pages/view';
+$route['(:any)'] = 'pages/view/$1';
+*/
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+
+$route['Affiche_Client/(:any)'] = 'Affiche_Client/view/$1';
+$route['Affiche_Client']='Affiche_Client';
+
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
--- application/views/views/pages/about.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-        <title>Chez Lei</title>
-    </head>
-    <body>
-
-        <h1>About Chez lei</h1>
-        <p>Chez lei est le meilleur magasin asiatic à Metz</p>
-
