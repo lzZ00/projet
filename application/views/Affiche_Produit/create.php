04b7f450ed4b87d6afcab0e1d851eec23084f830
zherui <zherui.li@outlook.com>
2017-04-14 16:54:11 +0200

    增加了添加图片功能，bug一堆

--- application/views/Affiche_Produit/create.php
+++ application/views/Affiche_Produit/create.php
@@ -15,7 +15,7 @@ $uptypes=array(
 
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('Affiche_Produit/createProduit'); ?>
+<?php echo form_open('Affiche_Produit/createProduit','id="create"'); ?>
 
 <div class="form-group">
     <label for="nom">Name</label>
@@ -41,64 +41,45 @@ $uptypes=array(
     <label for="type">Photo</label>
     <input type="input" class="form-control" id="photo" placeholder="Photo" name="photo"  style="width: 20%">
 </div>
+
 <div class="form-group">
         <input type="submit" name="submit" value="Create news item" class="btn btn-default" />
     </div>
 </div>
+</form>
 
+
+<!-- 存照片 -->
 <form enctype="multipart/form-data" method="post" name="upform">
     déposer le photo
     <input name="upfile" type="file">
     <input type="submit" value="Valider" name="Valider"><br>
     <?php
-    //il faut remplir le $nomdephoto à l'id_etu
-    $nomdephoto=$_POST['nom'];
-    $id=$_POST['id'];
-
-    if(isset($_POST['Valider'])){
+    if(isset($_POST['Valider'])) {
+        //il faut remplir le $nomdephoto à l'id_etu
         $file = $_FILES["upfile"];
-        $nom=$file['name'];
+        $nom = $file['name'];
         //echo "".$file['name'];
         //echo "</br>";
         //echo "".$file['type'];
-        $type=substr($nom,strpos($nom,'.'));
+        $type = substr($nom, strpos($nom, '.'));
         echo "</br>";
         //echo "".$type;
         echo "succece";
 
-        if(!in_array($file["type"], $uptypes))
-
-            //verifier le type de fichier
+        if (!in_array($file["type"], $uptypes)) //verifier le type de fichier
         {
-            echo "c'est pas une photo".$file["type"];
+            echo "c'est pas une photo" . $file["type"];
             exit;
         }
         //le nom de photo va changer à id_etu+$type
-        move_uploaded_file($_FILES["upfile"]["tmp_name"],
-            "photos/" .$nomdephoto.$type);
-
+        move_uploaded_file($_FILES["upfile"]["tmp_name"],"assets/img/" . $nom);
 
 
         echo "</br>";
-        $nomfinal="$nomdephoto$type";
-        echo "".$nomfinal;
-
-        //sauvegarder le nom de photo dans bdd
-        $objPdo->query("UPDATE Etudiant
-		SET photo='$nomfinal'
-		WHERE identifiant='$id'");
-        header ("location:?page=pageetu");
+        ?>
+        <td><img src="<?php echo base_url()?>assets/img/<?php echo $nom?>" width="50" alt="photo"/></td>
+        <?php
     }
-    //if l'etudent a deja met l'image
-    $photostocke=$objPdo->query("SELECT photo FROM Etudiant 
-	where identifiant='$id'");
-
-    if($photostocke!=null){
-        $photo = $photostocke->fetch();
-        echo "<img src=\"photos/". $photo['photo'] . "\"  width=\"200\" alt=\"photo\" />";
-
-    }
-
-
     ?>
 </form>
