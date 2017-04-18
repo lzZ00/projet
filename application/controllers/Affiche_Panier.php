ccdadf5aaa2f85928c2d88e09f0c6056631007ee
lzZ00 <310449968@qq.com>
2017-04-18 15:46:25 +0800

    购物车第一步

--- application/controllers/Affiche_Panier.php
+++ application/controllers/Affiche_Panier.php
@@ -5,3 +5,13 @@
  * Date: 2017/4/18
  * Time: 15:43
  */
+class Affiche_Painer extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Affiche_Produit_Model');
+        $this->load->model('Signup_Signin_Model');
+        $this->load->helper('url_helper');
+    }
+}
--- application/models/Affiche_Panier_Model.php
+++ application/models/Affiche_Panier_Model.php
@@ -5,3 +5,11 @@
  * Date: 2017/4/18
  * Time: 15:43
  */
+class Affiche_Panier_Model extends CI_Model
+{
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+}
